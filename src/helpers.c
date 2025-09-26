1a36f0165c1e2d4a19f59337a574c82278992970
Patryk Michalak <pmichalak@gmail.com>
2025-09-26 11:59:10 +0200

    bb

---
+++ src/helpers.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+typedef union U8x2 {
+    uint16_t u16;
+    uint8_t u8[2];
+} U8x2;
--- src/z80.c
+++ src/z80.c
@@ -1,14 +1,21 @@
 #include <stdint.h>
+
+const uint8_t FLAG_ZERO = 0x80;
+const uint8_t FLAG_SUBSTRACT = 0x40;
+const uint8_t FLAG_HALFCARRY = 0x20;
+const uint8_t FLAG_CARRY = 0x10;
+
+struct z80_clock {
+  int32_t m;
+  int32_t t;
+};
+
+struct z80_register {
+  uint8_t a, b, c, d, e, f, h, l;
+  uint16_t pc, sp;
+  int32_t m, t;
+};
 typedef struct z80 {
-  struct _clock {
-    int32_t m;
-    int32_t t;
-  };
-
-  struct _register {
-    uint8_t a, b, c, d, e, h, l, f;
-    uint16_t pc, sp;
-    int32_t m, t;
-  };
-
+  struct z80_clock _clock;
+  struct z80_register _register;
 } z80;
